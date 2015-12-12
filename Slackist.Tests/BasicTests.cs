ef50922b8969cc78eca70694f31148934e828164
Nyctef <nyctef@nyctef.com>
2015-12-12 19:17:52 +0000

    start playing around with websockets
    unfortunately it looks like this won't work on a mac, so back to windows
    we go :/

--- Slackist.Tests/BasicTests.cs
+++ Slackist.Tests/BasicTests.cs
@@ -1,10 +1,17 @@
 using Xunit;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class Tests
 {
 	[Fact]
-	public void TryUsingSockets()
+	public async Task TryUsingSockets()
 	{
-		
+		var client = new ClientWebSocket();
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+        await client.ConnectAsync(new Uri("ws://echo.websocket.org"), token);
 	}
 }
