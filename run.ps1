6ed6e7bb84c246ed84c07466a74c73b7d6bc52c0
Mark Jordan <mark.jordan@red-gate.com>
2015-12-17 23:29:39 +0000

    do a restore before running slackist
    This fixes the first run from a clean checkout

--- run.ps1
+++ run.ps1
@@ -1,7 +1,9 @@
 try {
+    $ErrorActionPreference = 'Stop'
     $slackToken = (get-content $PSScriptRoot/slack-token.txt)
     $todoistToken = (get-content $PSScriptRoot/todoist-token.txt)
     push-location $PSScriptRoot/Slackist
+    dnu restore
     dnx Slackist $slackToken $todoistToken
 }
 finally {
