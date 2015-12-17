182a6b72c4d3812a1c038844e4ddc4fec03b3510
Nyctef <nyctef@nyctef.com>
2015-12-17 22:40:34 +0000

    start fleshing out program.cs

--- Slackist/Program.cs
+++ Slackist/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Slackist
@@ -9,8 +10,30 @@ namespace Slackist
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Read();
+            MainAsync(args).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Usage: slackist [slack_api_token] [todoist_api_token]");
+                Environment.Exit(1);
+            }
+
+            var slackApiToken = args[0];
+            var todoistApiToken = args[1];
+
+            var cts = new CancellationTokenSource();
+            var cancel = cts.Token;
+
+            var todoistApi = new TodoistApi(todoistApiToken);
+            var watcher = new SlackStarredMessagesWatcher(slackApiToken, async message => {
+                Console.WriteLine("Message starred: " + message);
+                await todoistApi.CreateTask(message, "today", cancel);
+            });
+
+            await watcher.Watch(cancel);
         }
     }
 }
